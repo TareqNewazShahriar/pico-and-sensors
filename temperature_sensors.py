de1e6bbbc03a9852e54dab3d68caca7d75fe0fe5
TareqNewazShahriar <tareq.newaz.shahriar@gmail.com>
2022-09-02 03:26:52 +0600

    blink led for temperature sensor

--- temperature_sensors.py
+++ temperature_sensors.py
@@ -7,6 +7,13 @@ def convert_to_celcius(sensor_value):
    temperature = 27 - (reading - 0.706)/0.001721
    return temperature
 
+def blink_led(led):
+    for x in range(2):
+        led.value(1)
+        time.sleep(0.1)
+        led.value(0)
+        time.sleep(0.1)
+
 file = open ("temperature-data.csv", "a")
 file.write("Time, Build-in, Thermistor, Humiture, Temperature DS18b20\n")
 file.flush()
@@ -15,11 +22,10 @@ builtin_sensor = ADC(4)
 thermistor_sensor = ADC(2)
 humiture_sensor = ADC(1)
 temperature_sensor = ADC(0)
+led = Pin(25, Pin.OUT)
 
 delay = 60 * 5
 temp_val = 0
-
-print('ok')
 
 while True:
    temp_val = temperature_sensor.read_u16()
@@ -33,4 +39,5 @@ while True:
       temp_val)
    )
    file.flush()
+   blink_led(led)
    time.sleep(delay)
